d5f0903d3b38b851d0dcb5f93e1b7cac24dbd68a
mryou <mryou@maple.dti2.ne.jp>
2012-04-19 16:06:59 +0900

    時間関連のバグ修正

--- src/main.rb
+++ src/main.rb
@@ -1,3 +1,4 @@
+#! /usr/local/bin/ruby
 # -*- encoding: utf-8 -*-
 #
 # Vocalendar bot | Twitter Developers
@@ -13,25 +14,28 @@ require 'twitter'
 require 'pp'
 
 
-# Proxy設定
-proxy_host = ''
-proxy_port = 8080
+# Proxy設定（不要なら、proxy_hostをnilに設定
+proxy_host = '';
+proxy_port = 8080;
 
 # 認証設定
-oauth_token = ''
-oauth_token_secret = ''
+consumer_key = '';
+consumer_secret = '';
+oauth_token = '';
+oauth_token_secret = '';
 
 
 Net::HTTP.version_1_2;
 
 # 現在時間、いつからいつまでの予定を呟き対象とするかの時間
-now = DateTime.now().new_offset
+now = DateTime.now()
 totime = now + Rational(1, 24) * 4
 fromtime = totime - Rational(1, 24 * 60 ) * 9
 
+
 # カレンダーの予定の取得条件
 # 左から、開始時刻順、現在時刻からの予定、昇順、繰り返し予定を1つずつ取得
-query_hash = { 'orderby' => 'starttime', 'start-min' => now.strftime('%FT%TZ'), 'sortorder' => 'a', 'singleevents' => 'true'};
+query_hash = { 'orderby' => 'starttime', 'start-min' => now.strftime('%FT%T'), 'sortorder' => 'a', 'singleevents' => 'true'};
 query_string = query_hash.map{ |key,value|
 	"#{URI.encode(key)}=#{URI.encode(value)}" }.join("&");
 
@@ -62,11 +66,13 @@ end
 
 # Twitter ログイン
 Twitter.configure do |config|
-  config.consumer_key = 'YK5G2EDx0B9WNHQ7UilmQw'
-  config.consumer_secret = '0SgV8PgEvjUvXnXjiiH7pLe5Oq834zvRKsjDokxfYo'
+  config.consumer_key = consumer_key;
+  config.consumer_secret = consumer_secret;
   config.oauth_token = oauth_token
   config.oauth_token_secret = oauth_token_secret
-  config.proxy = 'http://' + proxy_host + ':' + proxy_port.to_s
+  if proxy_host != nil then
+	  config.proxy = 'http://' + proxy_host + ':' + proxy_port.to_s
+  end
 end
 
 # 予定の数だけ繰り返す
@@ -87,11 +93,10 @@ document.elements.each('feed/entry') do |entry|
 	starttimeStr = time.attributes['startTime'];
 	endtimeStr = time.attributes['endTime'];
 	timeEvent = starttimeStr.count('T') > 0;
-	if timeEvent then
-		starttime = DateTime.rfc3339( starttimeStr );
-	else
-		starttime = DateTime.strptime(starttimeStr, '%F');
+	if !timeEvent then
+		starttimeStr = starttimeStr + 'T00:00:00+09:00';
 	end
+	starttime = DateTime.rfc3339(starttimeStr);
 
 	puts title;
 	puts starttime;
@@ -104,7 +109,7 @@ document.elements.each('feed/entry') do |entry|
 		if timeEvent then
 			tweetsStr = starttime.strftime('%F %T') + 'は、' + title + 'の予定がありますよ。 ' + url + ' #vocalendar';
 		else
-			tweetsStr = starttime.strftime('%F') + 'は、' + title + 'の予定がありますよ。 " + url + ' #vocalendar';
+			tweetsStr = starttime.strftime('%F') + 'は、' + title + 'の予定がありますよ。 ' + url + ' #vocalendar';
 		end
 
 		Twitter.update(tweetsStr);
@@ -112,5 +117,6 @@ document.elements.each('feed/entry') do |entry|
 	end
 
 #	Twitter.update("テスト(´ー｀)")
+
 
 end
